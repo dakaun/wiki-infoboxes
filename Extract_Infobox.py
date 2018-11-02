602a5fee1aef9d541e0b75714ddf21ff00dc56de
Daniela Kaun <ugebn@student.kit.edu>
2018-11-02 14:54:57 +0100

    extract infobox with package mwparserfromhell

--- Extract_Infobox.py
+++ Extract_Infobox.py
@@ -2,13 +2,29 @@ import re
 # from lxml import etree # try to parse
 import mwparserfromhell
 
-with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/Second_Task/data/wiki_dump_short.txt',
-          encoding='cp65001') as articles_raw:
-    articles = articles_raw.read()
-    #article = mwparserfromhell.parse(articles)
-    #print(article)
-    #infobox =  article.filter_templates(matches='infobox .*')
 
-    # -----REGEX
-    infobox = re.findall(r'{{Infobox .*?}}', articles, re.DOTALL)
-    print(infobox)
+def open_wiki_articles():
+    article = ""
+    article_list = []
+    with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/Second_Task/data/wiki_dump_short.txt',
+              encoding='cp65001') as articles_raw:
+        articles_line = articles_raw.readline()
+        while articles_line:
+            article += articles_line
+            if '</page>' in articles_line:
+                article_list.append(article)
+                article = ""
+            articles_line = articles_raw.readline()
+    return article_list
+
+articles = open_wiki_articles()
+for text in articles:
+    article = mwparserfromhell.parse(text)
+    #print(article)
+    infobox = article.filter_templates(matches='infobox .*')
+    #print(infobox)
+    if infobox:
+        print('Name ', infobox[0].params[0].name)
+        print('Value ', infobox[0].params[0].value)
+
+
