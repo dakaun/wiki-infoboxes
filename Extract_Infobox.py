8685fcca8fca13dbeef258998922fc9f17eaa8d1
Daniela Kaun <ugebn@student.kit.edu>
2018-11-04 12:37:40 +0100

    create dic with all entities from the infoboxes

--- Extract_Infobox.py
+++ Extract_Infobox.py
@@ -17,14 +17,36 @@ def open_wiki_articles():
             articles_line = articles_raw.readline()
     return article_list
 
+def extract_title(article):
+    re_title = re.search(r'<title>.*?</title>', article)
+    group_title = re_title.group()
+    title = group_title.replace('<', '>').split('>')
+    return title[2]
+
 articles = open_wiki_articles()
+entity_dic = {}
+infobox_dic = {}
+name = ""
+value = ""
 for text in articles:
     article = mwparserfromhell.parse(text)
-    #print(article)
     infobox = article.filter_templates(matches='infobox .*')
-    #print(infobox)
+    article_title = extract_title(text)
+    # create dic with all linked entities from infobox
     if infobox:
-        print('Name ', infobox[0].params[0].name)
-        print('Value ', infobox[0].params[0].value)
+        for i in range(1, len(infobox[0].params)):
+            if '[[' in infobox[0].params[i].value:
+                print(infobox[0].params[i].value)
+                # clear name and value
+                name = str(infobox[0].params[i].name)
+                value = str(infobox[0].params[i].value)
+                name = name.replace('\n', '').replace(' ', '')
+                value = value.replace('\n', '').replace(' ', '')
+                entity_dic.update({name: value})
+        # infobox_dic = {title: {name:value},...}
+        infobox_dic.update({article_title: entity_dic})
+        entity_dic = {}
+print(infobox_dic)
+
 
 
