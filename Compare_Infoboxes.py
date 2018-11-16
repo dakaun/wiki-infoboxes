28d2f5ce81aa0bc799980f1335cc9f035a90e31d
Daniela Kaun <ugebn@student.kit.edu>
2018-11-16 12:13:29 +0100

    iterate through articles working

--- Compare_Infoboxes.py
+++ Compare_Infoboxes.py
@@ -1,34 +1,105 @@
 import Extract_Infobox
 import pandas as pd
 
-
-def open_triple_file():
-    with open(
-            r'C:\Users\danielak\Desktop\Dokumente Daniela\UNI\FIZ\Second_Task\test_wiki_crawler\short\wiki_triples(short).txt',
-            encoding='cp65001') as triple_f:
-        return triple_f
-
-
-entity_dic = Extract_Infobox.create_infobox_dic()
+infobox_path = Extract_Infobox.create_infobox_dic()
 with open(
         r'C:\Users\danielak\Desktop\Dokumente Daniela\UNI\FIZ\Second_Task\test_wiki_crawler\short\wiki_triples(short).txt',
         encoding='cp65001') as triple_f:
-    triple_line = ""
-    df = pd.DataFrame(columns=['Article', 'Entity', 'Sentence'])  # contains articles with infoboxes, and those entities which are links and were found in the article as links
-    for article in entity_dic:
+    with open(infobox_path) as infobox_f:
+        df = pd.DataFrame(columns=['Article', 'Entity',
+                                   'Sentence'])  # contains articles with infoboxes, and those entities which are links and were found in the article as links
+        infobox_f_line = infobox_f.readline()
+        #article = list(infobox_f_line.keys())[0]
         triple_line = triple_f.readline()
-        while (triple_line):
+        #triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+
+
+        while infobox_f_line and triple_line:
+            #print(infobox_f_line)
+            article = list(eval(infobox_f_line).keys())[0]
+            #print(article)
             triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
             if article == triple_article:
-                print(article, ': Artikel gefunden')
-                triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
-                for entity in entity_dic[article]:
-                    if triple_entity in entity_dic[article][entity]:
-                        print(triple_entity, ': Entity gefunden ')
-                        df = df.append({'Article': article, 'Entity': triple_entity, 'Sentence': triple_line.replace('>', '/').split('/')[10]}, ignore_index=True)
-                break
-            break
-print(df)
+                print('Article match: ', article)
+                #while article == triple_article:
+                #    entity = eval(infobox_f_line)
+                #    print(entity)
+                 #   triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
+                #    if triple_entity == entity:
+                #        print('nothing')
+
+
+                infobox_f_line = infobox_f.readline()
+                triple_line = triple_f.readline()
+            else:
+                triple_line = triple_f.readline()
+
+
+
+
+        # while infobox_f_line:
+        #     article = list(eval(infobox_f_line).keys())[0]
+        #     triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+        #     print(article)
+        #     if article == triple_article:
+        #         print('Article: ', article)
+        #     infobox_f_line = infobox_f.readline()
+        #     print(infobox_f_line)
+        #     triple_line = triple_f.readline()
+
+
+
+        #triple_line = triple_f.readline()
+        #triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+
+    # while triple_line:
+    #     triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+    #     for article in entity_dic:
+    #         while article == triple_article:
+    #             print('Article :', article)
+    # for entity in entity_dic[article]:
+    # print('Entity: ', entity)
+# article = ""
+# triple_line = triple_line = triple_f.readline()
+# triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+# for article in entity_dic:
+#     while article == triple_article:
+#         print('Article ', article)                triple_line = triple_line = triple_f.readline()
+#         triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+# triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
+# for entity in entity_dic[article]:
+# if triple_entity in entity_dic[article][entity]:
+
+
+# endlosscheife
+# for article, entity in entity_dic.items():
+#     print('Article: ', article)
+#     print('Entity: ', entity)
+
+# for entity in entity_dic[article]:
+#    print('Entity ', entity)
+# while article == triple_article:
+# print(article, ': Artikel gefunden')
+# triple_line = triple_line = triple_f.readline()
+# triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+# break
+
+#     while triple_line:
+#         for article in entity_dic:
+#             #triple_line = triple_f.readline()
+#             triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+#             while article == triple_article: # if article == triple_article:
+#                 print(article, ': Artikel gefunden')
+#                 triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
+#                 for entity in entity_dic[article]:
+#                     if triple_entity in entity_dic[article][entity]:
+#                         print(triple_entity, ': Entity gefunden ')
+#                         df = df.append({'Article': article, 'Entity': triple_entity,
+#                                         'Sentence': triple_line.replace('>', '/').split('/')[10]}, ignore_index=True)
+#                     triple_line = triple_f.readline()
+#                     triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
+#                 break
+# print(df)
 #
 # for line in triple_file:
 #     for article in entity_dic:
--- Extract_Infobox.py
+++ Extract_Infobox.py
@@ -1,6 +1,7 @@
 import re
 # from lxml import etree # try to parse
 import mwparserfromhell
+import datetime
 
 
 def open_wiki_articles():
@@ -17,11 +18,13 @@ def open_wiki_articles():
             articles_line = articles_raw.readline()
     return article_list
 
+
 def extract_title(article):
     re_title = re.search(r'<title>.*?</title>', article)
     group_title = re_title.group()
     title = group_title.replace('<', '>').split('>')
     return title[2]
+
 
 def create_infobox_dic():
     articles = open_wiki_articles()
@@ -29,25 +32,30 @@ def create_infobox_dic():
     infobox_dic = {}
     name = ""
     value = ""
-    for text in articles:
-        article = mwparserfromhell.parse(text)
-        infobox = article.filter_templates(matches='infobox .*')
-        article_title = extract_title(text)
-        # create dic with all linked entities from infobox
-        if infobox:
-            for i in range(1, len(infobox[0].params)):
-                if '[[' in infobox[0].params[i].value:
-                    # clear name and value
-                    name = str(infobox[0].params[i].name)
-                    value = str(infobox[0].params[i].value)
-                    name = name.replace('\n', '').replace(' ', '')
-                    value = value.replace('\n', '').replace(' ', '')
-                    entity_dic.update({name: value})
-            # infobox_dic = {title: {name:value},...}
-            infobox_dic.update({article_title: entity_dic})
-            entity_dic = {}
-    print(' -- Infobox successfully extracted')
-    return infobox_dic
+    path = 'C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/Second_Task/infobox_file/' + str(datetime.datetime.now().month) + str(datetime.datetime.now().day) + 'infobox.txt'
+    with open(path, 'w+') as infobox_file:
+        for text in articles:
+            infobox_dic = {}
+            article = mwparserfromhell.parse(text)
+            infobox = article.filter_templates(matches='infobox .*')
+            article_title = extract_title(text)
+            # create dic with all linked entities from infobox
+            if infobox:
+                entity_dic = {}
+                for i in range(1, len(infobox[0].params)):
+                    if '[[' in infobox[0].params[i].value:
+                        # clear name and value
+                        name = str(infobox[0].params[i].name)
+                        value = str(infobox[0].params[i].value)
+                        name = name.replace('\n', '').replace(' ', '')
+                        value = value.replace('\n', '').replace(' ', '')
+                        entity_dic.update({name: value})
+                # infobox_dic = {title: {name:value},...}
+                infobox_dic.update({article_title: entity_dic})
+                infobox_file.write(str(infobox_dic) + '\n')
+                entity_dic = {}
+    return path
 
 
-
+if __name__ == '__main__':
+    create_infobox_dic()
