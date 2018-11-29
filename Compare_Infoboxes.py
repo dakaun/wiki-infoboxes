87e47c30f456297e59952151925835515c5d1001
Daniela Kaun <ugebn@student.kit.edu>
2018-11-29 10:14:09 +0100

    iterating working

--- Compare_Infoboxes.py
+++ Compare_Infoboxes.py
@@ -26,18 +26,23 @@ with open(
                     triple_entity = triple_line.replace('>', '/').split('/')[9].replace('_', '')
                     print('Triple Entity: ', triple_entity)
                     print('Infobox Entity: ', eval(infobox_f_line)[article][infobox_entity_list[0]])
-                    if triple_entity in eval(infobox_f_line)[article][infobox_entity_list[0]]:
-                        df = df.append({'Article': article, 'Entity': triple_entity,
-                                        'Sentence': triple_line.replace('>', '/').split('/')[10]}, ignore_index=True)
-                        df.to_csv(
-                            r'C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/Second_Task/result_match/' + str(
-                                datetime.datetime.now().month) + str(
-                                datetime.datetime.now().day) + 'infobox_matches.csv', index=False)
-                        infobox_entity_list.pop(0)
-                        triple_line = triple_f.readline()
+                    # check if infobox entity is in triple file, otherwise take next infobox entity
+                    entity = re.search(r'/' + triple_entity + '>', triple_file)
+                    if entity:
+                        if triple_entity in eval(infobox_f_line)[article][infobox_entity_list[0]]:
+                            df = df.append({'Article': article, 'Entity': triple_entity,
+                                            'Sentence': triple_line.replace('>', '/').split('/')[10]}, ignore_index=True)
+                            df.to_csv(
+                                r'C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/Second_Task/result_match/' + str(
+                                    datetime.datetime.now().month) + str(
+                                    datetime.datetime.now().day) + 'infobox_matches.csv', index=False)
+                            infobox_entity_list.pop(0)
+                            triple_line = triple_f.readline()
+                        else:
+                            triple_line = triple_f.readline()
+                        triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
                     else:
-                        triple_line = triple_f.readline()
-                    triple_article = triple_line.replace('>', '/').split('/')[4].replace('_', ' ')
+                        infobox_entity_list.pop(0)
                 # back to article rotating
                 infobox_f_line = infobox_f.readline()
                 triple_line = triple_f.readline()
