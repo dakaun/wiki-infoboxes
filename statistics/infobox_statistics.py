7515479ef38a8c991424d082376e3b95dc0b2706
Daniela Kaun <ugebn@student.kit.edu>
2019-01-25 15:18:49 +0100

    statistics on files

--- statistics/infobox_statistics.py
+++ statistics/infobox_statistics.py
@@ -1,23 +1,49 @@
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-comp_path', help='Path to result file which contains all counter information')
-args = parser.parse_args()
-path = args.comp_path
+#parser = argparse.ArgumentParser()
+#parser.add_argument('-comp_path', help='Path to result file which contains all counter information')
+#args = parser.parse_args()
+#path = args.comp_path
 # path= '../../../comp/1216_info_comp.csv'
 
-data = pd.read_csv(path , sep=';')
-sum_amount_values = data['amount_values'].sum(0)
-sum_amount_entities = data['amount_entities'].sum(0)
-sum_amount_links = data['amount_links'].sum(0)
-sum_amount_link_article_match = data['amount_link_article_match'].sum(0)
-print(f'Sum of all given entities in the infoboxes: {sum_amount_values} (included those, which are given by the template, but are not used)')
-print(f'Sum of all values: {sum_amount_entities}')
-print(f'Sum of all values, which contain a link: {sum_amount_links}')
-print(f'Sum of all values, which links also appear in the article as link: {sum_amount_link_article_match}')
-print(f'{(sum_amount_entities/sum_amount_values)*100} % of all given infobox entites contain a value.')
-# based on sum_amount_entities - only those entites which contain something
-print(f'{(sum_amount_links/sum_amount_entities)*100} % of all infobox values contain a link.')
-print(f'{(sum_amount_link_article_match/sum_amount_entities)*100} % of the infobox values are links which appear in the article .')
-print(f'{(sum_amount_link_article_match/sum_amount_links)*100} % of all links appear in the article')
+data1 = pd.read_csv('../data/comp_infobox/res/124_comp.csv', sep=';')
+data2 = pd.read_csv('../data/comp_infobox/res/1801_comp_918596.csv', sep=';')
+data3 = pd.read_csv('../data/comp_infobox/res/2012_comp_small.csv', sep=';')
+
+print(data1.shape)
+print(data2.shape)
+print(data3.shape)
+
+# concat total df
+frame  = [data1, data2, data3] # add data
+data_total = pd.concat(frame, axis=0, ignore_index=True)
+
+print("TOTAL DATA SHAPE: " + data1.shape)
+# data_total:
+print("There are " + str(data_total['article'].nunique()) +" unique articles.")
+data_total = data_total[data_total['article'].duplicated()!=True] # todo how many infoboxes are there
+
+amount_entites = data_total['amount_entities'].sum()
+amount_link_article_match = data_total['amount_link_article_match'].sum()
+amount_links = data_total['amount_links'].sum()
+amount_properties = data_total['amount_properties'].sum()
+#amount_values = data_total['amount_values'].sum()
+
+print(f'Amount Properties: {amount_properties}')
+print(f'Amount Values: {amount_entites}')
+print(f'Amount Links: {amount_links}')
+print(f'Amount Links which appear in the articles {amount_link_article_match}')
+
+print(f'The Infobox Templates contain {amount_properties} properties')
+print(f'{round(amount_entites/amount_properties *100, 2)} % of these properties do have a value. (entities/properties)')
+print(f'{round(amount_links/amount_entites *100, 2)} % of those values are links. (links/entities)')
+print(f'{round(amount_link_article_match/amount_links *100, 2)} % of those links appear in the article. (match/link)')
+print(f'{round(amount_link_article_match/amount_entites*100, 2)} % of all values are links which appear in the article, too. (match/entities)')
+
+#infobox
+column_link_match = data_total["amount_link_article_match"]
+column_link_match = column_link_match.replace(0, pd.np.nan).dropna()
+print("There are " + str(column_link_match) + " infoboxes with links, which appear in the articles too.")
+print("Mean: " + str(column_link_match.mean()))
+
